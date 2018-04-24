3fa135c2bd613405e5611ef4e178b4efb024218f
cryll <likan_95@hotmail.es>
2018-04-24 11:52:08 +0200

    prueba
    No me gustan los numeros impares

--- src/utiles/Constantes.java
+++ src/utiles/Constantes.java
@@ -3,7 +3,7 @@ package utiles;
 public final class Constantes {
 
 	public final static int TAMANO_COLA=10;
-	public final static int TAMANO_PILA=5;
+	public final static int TAMANO_PILA=4;
 	public final static int TAMANO_LISTA_LADO=5;
 	public final static int CANTIDAD_MAX_MONEDAS=20;
 	public final static int PETICION_COLOR_MAX=3;
